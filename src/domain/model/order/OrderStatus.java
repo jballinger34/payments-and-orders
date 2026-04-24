09f92063c3bd64b37afc219c3c4b4eabc542cd7c
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-04-24 11:10:43 +0100

    Redefined OrderStatus - provided more info on overall flow - now need to implement all stages

--- src/domain/model/order/OrderStatus.java
+++ src/domain/model/order/OrderStatus.java
@@ -1,9 +1,18 @@
 package domain.model.order;
 
 public enum OrderStatus {
-    CREATED,
-    PAID,
-    COMPLETED,
-    REFUNDED,
-    CANCELLED,
+    CREATED, //order intially created - has payment attached but not auth yet
+    READY, //order ready - user payment has been auth, but stock not yet reserved
+    RESERVED, //stock has been reserved - need to capture payment
+    PAID, // payment captured - now its on the merchant to fulfil the order
+    PROCESSING, // process of fulfilling the order - building, in shipping, etc.
+    FULFILLED, // order fulfilled - customer has their order - but it is still refundable
+
+    COMPLETED, // order fully completed - customer can no longer refund payment
+
+    CANCELLED, // order canceled BEFORE PAID state - no stock, auth declined, etc.
+
+    REFUND_REQUESTED, // order reverted AFTER PAYMENT RECEIVED - waiting on confirmation
+    REFUND_CONFIRMED, // merchant has confirmed that they have received goods back or are otherwise ready to give customer refund
+    REFUNDED, // order refunded - merchant has sent money back to customer - in finished state like CANCELLED and COMPLETED
 }
