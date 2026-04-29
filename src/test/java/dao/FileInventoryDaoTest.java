7ba3998e98d81ad8ac01927babdee3a5d68421d4
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-04-29 12:27:23 +0100

    Added test for load/save logic for Inventory File Dao

--- src/test/java/dao/FileInventoryDaoTest.java
+++ src/test/java/dao/FileInventoryDaoTest.java
@@ -5,26 +5,59 @@ import me.jamie.paymentspractice.dao.inventory.InventoryDao;
 import me.jamie.paymentspractice.exception.DuplicateProductException;
 import me.jamie.paymentspractice.exception.PersistenceException;
 import me.jamie.paymentspractice.exception.ProductNotFoundException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FileInventoryDaoTest {
 
     private InventoryDao testDao;
+    private final String testFile = "test_inventory.txt";
 
     @BeforeEach
     void setUp() throws IOException {
-        String testFile = "test_inventory.txt";
         //blank out file
         new FileWriter(testFile);
         testDao = new FileInventoryDao(testFile);
     }
+
+    @Test
+    void testSaveToFile() throws PersistenceException {
+        String productId = "TEST_PRODUCT_1";
+        testDao.addProduct(productId);
+        testDao.alterStock(productId,10);
+
+        Scanner scanner = new Scanner(testFile);
+        String entry = scanner.nextLine();
+
+        String[] productIdAndQuantity = entry.split("::");
+        assertEquals(productId, productIdAndQuantity[0]);
+        assertEquals("10", productIdAndQuantity[1]);
+    }
+    @Test
+    void testLoadFile() throws PersistenceException {
+        String productId = "TEST_PRODUCT_1";
+        int quantity = 10;
+        String testEntry = productId + "::" + quantity;
+        try (FileWriter fr = new FileWriter(testFile)){
+            fr.append(testEntry);
+
+            //CONSTRUCTOR CALLS THE LOAD METHOD THAT WE ARE TESTING
+            testDao = new FileInventoryDao(testFile);
+            int quantityFound = testDao.getStock(productId);
+            assertEquals(quantity, quantityFound);
+
+        } catch (IOException e){
+            fail("Test setup failed due to IOException: " + e.getMessage());
+        }
+    }
+
 
     @Test
     void testAddGetDefaultStock() throws PersistenceException {
