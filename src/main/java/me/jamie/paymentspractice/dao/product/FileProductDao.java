34767458604d472a4b3f80be70476dc348b82afe
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-04-30 14:14:14 +0100

    added FileProductDao implementation of ProductDao. now need to let merchants create products and manage their inventory

--- src/main/java/me/jamie/paymentspractice/dao/product/FileProductDao.java
+++ src/main/java/me/jamie/paymentspractice/dao/product/FileProductDao.java
@@ -1,41 +1,94 @@
 package me.jamie.paymentspractice.dao.product;
 
 import me.jamie.paymentspractice.domain.model.Product;
+import me.jamie.paymentspractice.exception.InvalidDataException;
+import me.jamie.paymentspractice.exception.PersistenceException;
+import me.jamie.paymentspractice.exception.ProductNotFoundException;
 
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class FileProductDao implements ProductDao {
 
     private final String PRODUCTS_FILE;
+    private final String DELIMITER = "::";
 
-    public FileProductDao(String products_file){
+    private final Map<String,Product> products = new HashMap<>();
+
+
+    public FileProductDao(String products_file) throws PersistenceException {
         this.PRODUCTS_FILE = products_file;
+        loadProducts();
     }
 
-    private void loadProducts(){
+    private void loadProducts() throws PersistenceException {
+        //get file from String, check if it exists, if not create it
+        //get Scanner to read it
+        if(PRODUCTS_FILE == null) return;
+        File f = new File(PRODUCTS_FILE);
+        Scanner s;
+        try{
+            if(!f.exists()){
+                f.createNewFile();
+            }
+            s = new Scanner(new FileReader(PRODUCTS_FILE));
+        } catch (IOException e){
+            throw new PersistenceException("Error loading products file.", e);
+        }
 
+        //saved in format
+        //id::name::cost
+        while(s.hasNextLine()) {
+            try{
+                String entry = s.nextLine();
+                String[] fields = entry.split(DELIMITER);
+                String productId = fields[0];
+                String name = fields[1];
+                double cost = Double.parseDouble(fields[2]);
+                products.put(productId, new Product(productId,name,cost));
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e){
+                throw new InvalidDataException("Tried to load invalid product. Possible data corruption.", e);
+            }
+        }
     }
-    private void writeAllProducts(){
+    private void writeAllProducts() throws PersistenceException {
+        try (PrintWriter out = new PrintWriter(new FileWriter(PRODUCTS_FILE))){
 
+            for(Product product : products.values()){
+                out.println(product.getId() + DELIMITER + product.getName() + DELIMITER + product.getCost());
+            }
+            out.flush();
+
+        } catch (IOException e) {
+            throw new PersistenceException("Could not save products", e);
+        }
     }
 
     @Override
-    public void put(Product product) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public void put(Product product) throws PersistenceException {
+        products.put(product.getId(), product);
+        writeAllProducts();
     }
 
     @Override
-    public void remove(String productId) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public void remove(String productId) throws PersistenceException {
+        if(!products.containsKey(productId)){
+            throw new ProductNotFoundException("Tried to remove product " + productId + " that doesn't exist.");
+        }
+        products.remove(productId);
+        writeAllProducts();
     }
 
     @Override
     public Product findById(String productId) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if(!products.containsKey(productId)){
+            throw new ProductNotFoundException("Product " + productId + " doesn't exist.");
+        }
+        return products.get(productId);
     }
 
     @Override
     public List<Product> findAll() {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return new ArrayList<>(products.values());
     }
 }
