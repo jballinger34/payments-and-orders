62784ba1ef5a35eab086b04c3b0670516d2910fd
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-04-30 14:03:05 +0100

    Tests for FileProductDao

--- src/main/java/me/jamie/paymentspractice/dao/product/ProductDao.java
+++ src/main/java/me/jamie/paymentspractice/dao/product/ProductDao.java
@@ -1,16 +1,18 @@
 package me.jamie.paymentspractice.dao.product;
 
 import me.jamie.paymentspractice.domain.model.Product;
+import me.jamie.paymentspractice.exception.PersistenceException;
+import me.jamie.paymentspractice.exception.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductDao {
 
-    void put(Product product);
-    void remove(String productId);
+    void put(Product product) throws PersistenceException;
+    void remove(String productId) throws PersistenceException, ProductNotFoundException;
 
-    Product findById(String productId);
-    List<Product> findAll();
+    Product findById(String productId) throws PersistenceException, ProductNotFoundException;
+    List<Product> findAll() throws PersistenceException;
 
 
 }
--- src/test/java/dao/FileProductDaoTest.java
+++ src/test/java/dao/FileProductDaoTest.java
@@ -1,16 +1,18 @@
 package dao;
 
-import me.jamie.paymentspractice.dao.inventory.FileInventoryDao;
 import me.jamie.paymentspractice.dao.product.FileProductDao;
 import me.jamie.paymentspractice.dao.product.ProductDao;
+import me.jamie.paymentspractice.domain.model.Product;
 import me.jamie.paymentspractice.exception.PersistenceException;
+import me.jamie.paymentspractice.exception.ProductNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FileProductDaoTest {
 
@@ -25,28 +27,127 @@ public class FileProductDaoTest {
     }
 
     @Test
-    void testLoadFromFile(){
-        fail("TEST NOT WRITTEN");
-    }
-    @Test
     void testSaveToFile(){
-        fail("TEST NOT WRITTEN");
+        String productId = "TEST_PRODUCT_1";
+        Product product = new Product(productId, productId, 1);
+
+        try(Scanner scanner = new Scanner(new FileReader(testFile))){
+            testDao.put(product);
+            String entry = scanner.nextLine();
+
+            String[] tokens = entry.split("::");
+            assertEquals(productId, tokens[0]);
+            assertEquals(productId, tokens[1]);
+            assertEquals(1, Integer.parseInt(tokens[2]));
+        } catch (IOException | PersistenceException e){
+            fail("Test setup failed due to exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testLoadFromFile(){
+        String productId = "TEST_PRODUCT_1";
+        int cost = 10;
+        String testEntry = productId + "::" + productId + "::" + cost;
+        try (PrintWriter out = new PrintWriter(new FileWriter(testFile))){
+            out.println(testEntry);
+            out.flush();
+            //CONSTRUCTOR CALLS THE LOAD METHOD THAT WE ARE TESTING
+            testDao = new FileProductDao(testFile);
+            Product product = testDao.findById(productId);
+            //normally name wouldnt be id but here we just set name/id to be the same
+            assertEquals(productId, product.getName());
+            assertEquals(cost, product.getCost());
+
+        } catch (IOException | PersistenceException e){
+            fail("Test setup failed due to exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testAddFindProduct(){
+        String productId = "TEST_PRODUCT_1";
+        Product product = new Product(productId, "A Product", 10);
+        try {
+            testDao.put(product);
+
+            assertEquals(productId, testDao.findById(productId).getId());
+            assertEquals(product.getName(), testDao.findById(productId).getName());
+            assertEquals(product.getCost(), testDao.findById(productId).getCost());
+        } catch (PersistenceException e){
+            fail("Test setup failed due to persistance exception: " + e.getMessage());
+        }
+
     }
     @Test
-    void testAddNewProduct(){
-        fail("TEST NOT WRITTEN");
+    void testUpdateFindProduct(){
+        String productId = "TEST_PRODUCT_1";
+        try{
+            testDao.put(new Product(productId, "A Product", 10));
+            Product product1 = new Product(productId, "Renamed product", 100);
+            testDao.put(product1);
+
+            assertEquals(productId, testDao.findById(productId));
+            assertEquals(product1.getName(), testDao.findById(productId).getName());
+            assertEquals(product1.getCost(), testDao.findById(productId).getCost());
+        } catch (PersistenceException e){
+            fail("Test setup failed due to persistance exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testRemoveProduct(){
+        String productId = "TEST_PRODUCT_1";
+        try{
+            testDao.put(new Product(productId,productId,10));
+            testDao.remove(productId);
+            assertThrows(ProductNotFoundException.class, () -> testDao.findById(productId));
+
+        } catch (PersistenceException e){
+            fail("Test setup failed due to persistance exception: " + e.getMessage());
+        }
     }
     @Test
-    void testUpdateProduct(){
-        fail("TEST NOT WRITTEN");
+    void testRemoveProductNotFound(){
+        String productId = "NOT_A_REAL_PRODUCT";
+        assertThrows(ProductNotFoundException.class, () -> testDao.remove(productId));
+    }
+
+    @Test
+    void testFindProductNotFound() {
+        try {
+            testDao.put(new Product("TEST_PRODUCT_1", "A Product", 10));
+            assertThrows(ProductNotFoundException.class, () -> testDao.findById("TEST_PRODUCT_2"));
+        } catch (PersistenceException e) {
+            fail("Test setup failed due to persistance exception: " + e.getMessage());
+        }
     }
     @Test
-    void testFindById(){
-        fail("TEST NOT WRITTEN");
+    void testFindAllEmpty(){
+        try{
+            List<Product> productList = testDao.findAll();
+
+            assertNotNull(productList);
+            assertEquals(0, productList.size());
+        } catch (PersistenceException e) {
+            fail("Test setup failed due to persistance exception: " + e.getMessage());
+        }
     }
+
     @Test
     void testFindAll(){
-        fail("TEST NOT WRITTEN");
+        try {
+            String duplicateId = "DUPLICATE_PRODUCT";
+            testDao.put(new Product(duplicateId, duplicateId, 5));
+            //duplicate below, will update
+            testDao.put(new Product(duplicateId, "new_name", 10));
+            testDao.put(new Product("TEST_PRODUCT_1", "name", 10));
+            testDao.put(new Product("TEST_PRODUCT_2", "name", 10));
+
+            assertEquals(3, testDao.findAll().size());
+        } catch (PersistenceException e){
+            fail("Test setup failed due to persistance exception: " + e.getMessage());
+        }
     }
 
 
