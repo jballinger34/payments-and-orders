209f82f78208a8b400acd97fb15c1ff7adcbf6ae
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-05-08 15:18:44 +0100

    Implemented Order Contoller, allows merchant to view order or all orders, as well as mark orders as fulfilled or completed

---
+++ src/main/java/me/jamie/paymentspractice/controller/OrderController.java
@@ -0,0 +1,46 @@
+package me.jamie.paymentspractice.controller;
+
+import me.jamie.paymentspractice.domain.model.order.Order;
+import me.jamie.paymentspractice.dto.OrderDto;
+import me.jamie.paymentspractice.exception.PersistenceException;
+import me.jamie.paymentspractice.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    OrderService orderService;
+
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    //TODO pass status to filter by status
+
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> getAllOrders() throws PersistenceException {
+        List<OrderDto> orderDtos = orderService.getAllOrders().stream().map(OrderDto::from).toList();
+        return ResponseEntity.ok(orderDtos);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDto> getOrder(@PathVariable String id) throws PersistenceException {
+        OrderDto orderDto = orderService.getOrder(id);
+        return ResponseEntity.ok(orderDto);
+    }
+
+    @PutMapping("/{id}/fulfil")
+    public ResponseEntity<OrderDto> markFulfilled(@PathVariable String id) throws PersistenceException {
+        OrderDto dto = orderService.fulfil(id);
+        return ResponseEntity.ok(dto);
+    }
+    @PutMapping("/{id}/complete")
+    public ResponseEntity<OrderDto> markCompleted(@PathVariable String id) throws PersistenceException {
+        OrderDto dto = orderService.complete(id);
+        return ResponseEntity.ok(dto);
+    }
+
+}
