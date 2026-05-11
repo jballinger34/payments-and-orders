9368e63486e2dd588aab6c59bc887d7438cb048e
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-05-11 14:38:03 +0100

    Added a GlobalExceptionHandler to deal with thrown exceptions from RestControllers.

---
+++ src/main/java/me/jamie/paymentspractice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,72 @@
+package me.jamie.paymentspractice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error",  e.getEntityType() + " Not Found",
+                        "message", e.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(PersistenceException.class)
+    public ResponseEntity<?> handlePersistenceException(PersistenceException e){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error", "Persistence Error",
+                        "message", e.getMessage()
+                ));
+
+    }
+
+    @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<?> handleInsufficientStockException(InsufficientStockException e){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error", "Insufficient Stock",
+                        "message", e.getMessage()
+                ));
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalStateException e){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error", "Request invalid because of current state",
+                        "message", e.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "error", "Invalid input",
+                        "message", e.getMessage()
+                ));
+    }
+
+
+
+
+}
