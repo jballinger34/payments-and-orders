8324b31f4597d597a39aa1e2860a5c9cd2d19951
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-05-19 10:40:39 +0100

    Updates to service layer to fit daos that use merchantId

--- src/main/java/me/jamie/paymentspractice/service/InventoryService.java
+++ src/main/java/me/jamie/paymentspractice/service/InventoryService.java
@@ -1,6 +1,7 @@
 package me.jamie.paymentspractice.service;
 
 import me.jamie.paymentspractice.dao.inventory.InventoryDao;
+import me.jamie.paymentspractice.data.entity.ProductEntity;
 import me.jamie.paymentspractice.domain.model.Product;
 import me.jamie.paymentspractice.exception.InsufficientStockException;
 import me.jamie.paymentspractice.exception.PersistenceException;
@@ -24,23 +25,23 @@ public class InventoryService {
         this.auditService = auditService;
     }
 
-    public int getStockQty(String productId) throws PersistenceException, ProductNotFoundException {
-        return inventoryDao.findById(productId).getStock();
+    public int getStockQty(String merchantId, String productId) throws PersistenceException, ProductNotFoundException {
+        return inventoryDao.getStock(merchantId, productId);
     }
-    public boolean isInStock(String productId, int quantity) throws PersistenceException, ProductNotFoundException {
-        int amtInStock = inventoryDao.findById(productId).getStock();
+    public boolean isInStock(String merchantId, String productId, int quantity) throws PersistenceException, ProductNotFoundException {
+        int amtInStock = inventoryDao.getStock(merchantId,productId);
         return quantity <= amtInStock;
     }
-    public Product createProduct(String name, double cost, int stock) throws PersistenceException {
+    public Product createProduct(String merchantId, String name, double cost, int stock) throws PersistenceException {
         if(stock < 0) throw new IllegalArgumentException("Stock cannot be negative");
         if(cost < 0) throw new IllegalArgumentException("Cost cannot be negative");
 
         String id = UUID.randomUUID().toString();
-        Product product = new Product(id, name, cost, stock);
+        Product product = new Product(id, merchantId, name, cost, stock);
         auditService.logAttempt(AuditType.INVENTORY, AuditAction.CREATE, id);
 
         try {
-            inventoryDao.put(id, product);
+            inventoryDao.save(ProductEntity.from(product));
             auditService.logSuccess(AuditType.INVENTORY, AuditAction.CREATE, id);
             return product;
 
@@ -50,13 +51,13 @@ public class InventoryService {
         }
 
     }
-    public Product setName(String productId, String name) throws PersistenceException, ProductNotFoundException {
-        Product product = inventoryDao.findById(productId);
+    public Product setName(String merchantId, String productId, String name) throws PersistenceException, ProductNotFoundException {
+        Product product = Product.fromPersistence(inventoryDao.findByMerchantIdAndId(merchantId, productId));
         auditService.logAttempt(AuditType.INVENTORY, AuditAction.SET_NAME, productId);
 
         try {
             product.setName(name);
-            inventoryDao.put(productId, product);
+            inventoryDao.save(ProductEntity.from(product));
             auditService.logSuccess(AuditType.INVENTORY, AuditAction.SET_NAME, productId);
             return product;
 
@@ -66,14 +67,14 @@ public class InventoryService {
         }
 
     }
-    public Product setPrice(String productId, double price) throws PersistenceException, ProductNotFoundException {
+    public Product setPrice(String merchantId, String productId, double price) throws PersistenceException, ProductNotFoundException {
         if (price < 0) throw new IllegalArgumentException("Cost cannot be negative");
-        Product product = inventoryDao.findById(productId);
+        Product product = Product.fromPersistence(inventoryDao.findByMerchantIdAndId(merchantId, productId));
         auditService.logAttempt(AuditType.INVENTORY, AuditAction.SET_COST, productId);
 
         try {
             product.setCost(price);
-            inventoryDao.put(productId, product);
+            inventoryDao.save(ProductEntity.from(product));
             auditService.logSuccess(AuditType.INVENTORY, AuditAction.SET_COST, productId);
             return product;
 
@@ -83,15 +84,15 @@ public class InventoryService {
         }
 
     }
-    public Product setStock(String productId, int stock) throws PersistenceException, ProductNotFoundException {
+    public Product setStock(String merchantId, String productId, int stock) throws PersistenceException, ProductNotFoundException {
         if (stock < 0) throw new IllegalArgumentException("Stock cannot be negative");
 
-        Product product = inventoryDao.findById(productId);
+        Product product = Product.fromPersistence(inventoryDao.findByMerchantIdAndId(merchantId, productId));
         auditService.logAttempt(AuditType.INVENTORY, AuditAction.SET_STOCK, productId);
 
         try {
             product.setStock(stock);
-            inventoryDao.put(productId, product);
+            inventoryDao.save(ProductEntity.from(product));
             auditService.logSuccess(AuditType.INVENTORY, AuditAction.SET_STOCK, productId);
             return product;
 
@@ -101,13 +102,13 @@ public class InventoryService {
         }
     }
 
-    public Product restockProduct(String productId, int amount) throws PersistenceException, ProductNotFoundException{
+    public Product restockProduct(String merchantId, String productId, int amount) throws PersistenceException, ProductNotFoundException{
         if(amount <= 0) throw new IllegalArgumentException("Restock amount must be positive.");
-        Product product = inventoryDao.findById(productId);
+        Product product = Product.fromPersistence(inventoryDao.findByMerchantIdAndId(merchantId, productId));
         auditService.logAttempt(AuditType.INVENTORY, AuditAction.RESTOCK, productId);
         try{
             product.setStock(product.getStock()+amount);
-            inventoryDao.put(productId, product);
+            inventoryDao.save(ProductEntity.from(product));
             auditService.logSuccess(AuditType.INVENTORY, AuditAction.RESTOCK, productId);
             return product;
         } catch (Exception e){
@@ -117,32 +118,32 @@ public class InventoryService {
 
     }
 
-    public Product reduceStock(String productId, int quantity) throws PersistenceException {
+    public Product reduceStock(String merchantId, String productId, int quantity) throws PersistenceException {
         if(quantity <= 0) throw new IllegalArgumentException("Invalid argument, cannot reduce stock by non-positive number " + quantity );
-        Product product = inventoryDao.findById(productId);
-        if(!isInStock(productId,quantity)) throw new InsufficientStockException("Insufficient stock of product, id: " + productId);
+        Product product = Product.fromPersistence(inventoryDao.findByMerchantIdAndId(merchantId, productId));
+        if(!isInStock(merchantId,productId,quantity)) throw new InsufficientStockException("Insufficient stock of product, id: " + productId);
         auditService.logAttempt(AuditType.INVENTORY, AuditAction.REDUCE_STOCK, productId);
 
         try{
             product.setStock(product.getStock() - quantity);
-            inventoryDao.put(productId, product);
+            inventoryDao.save(ProductEntity.from(product));
             auditService.logSuccess(AuditType.INVENTORY, AuditAction.REDUCE_STOCK, productId);
             return product;
         } catch (Exception e){
             auditService.logFailure(AuditType.INVENTORY, AuditAction.REDUCE_STOCK, productId, e.getMessage());
             throw e;
         }
+    }
+    public List<Product> getAllProducts(String merchantId) throws PersistenceException {
+        return inventoryDao.findByMerchantId(merchantId).stream().map(Product::fromPersistence).toList();
 
-
     }
-    public List<Product> getAllProducts() throws PersistenceException {
-        return inventoryDao.findAll();
-    }
-    public Product getProduct(String productId) throws PersistenceException, ProductNotFoundException{
-        return inventoryDao.findById(productId);
+    public Product getProduct(String merchantId, String productId) throws PersistenceException, ProductNotFoundException{
+        return Product.fromPersistence(inventoryDao.findByMerchantIdAndId(merchantId, productId));
     }
 
-    public void deleteProduct(String id) throws PersistenceException {
-        inventoryDao.remove(id);
+    public void deleteProduct(String merchantId, String productId) throws PersistenceException {
+        inventoryDao.remove(merchantId, productId);
     }
+
 }
--- src/main/java/me/jamie/paymentspractice/service/OrderService.java
+++ src/main/java/me/jamie/paymentspractice/service/OrderService.java
@@ -2,7 +2,9 @@ package me.jamie.paymentspractice.service;
 
 
 import me.jamie.paymentspractice.dao.order.OrderDao;
+import me.jamie.paymentspractice.data.entity.LineItemEntity;
 import me.jamie.paymentspractice.domain.model.LineItem;
+import me.jamie.paymentspractice.domain.model.Merchant;
 import me.jamie.paymentspractice.domain.model.Product;
 import me.jamie.paymentspractice.domain.model.order.Order;
 import me.jamie.paymentspractice.domain.model.order.OrderStatus;
@@ -10,9 +12,8 @@ import me.jamie.paymentspractice.domain.model.payment.Payment;
 import me.jamie.paymentspractice.domain.model.payment.PaymentMethod;
 import me.jamie.paymentspractice.domain.model.payment.PaymentStatus;
 import me.jamie.paymentspractice.data.request.CheckoutItemRequest;
-import me.jamie.paymentspractice.data.record.LineItemRecord;
 import me.jamie.paymentspractice.data.dto.OrderDto;
-import me.jamie.paymentspractice.data.record.OrderRecord;
+import me.jamie.paymentspractice.data.entity.OrderEntity;
 import me.jamie.paymentspractice.exception.InsufficientStockException;
 import me.jamie.paymentspractice.exception.OrderNotFoundException;
 import me.jamie.paymentspractice.exception.PersistenceException;
@@ -28,7 +29,9 @@ public class OrderService {
     OrderDao dao;
     PaymentService paymentService;
     InventoryService inventoryService;
+    MerchantService merchantService;
     AuditService auditService;
+
 
     public OrderService(OrderDao orderDao, PaymentService paymentService, InventoryService inventoryService, AuditService auditService){
         this.dao = orderDao;
@@ -36,50 +39,53 @@ public class OrderService {
         this.inventoryService = inventoryService;
         this.auditService = auditService;
     }
-    public OrderDto getOrder(String id) throws PersistenceException, OrderNotFoundException {
-        return OrderDto.from(buildOrder(dao.findById(id)));
+    public OrderDto getOrder(String merchantId, String orderId) throws PersistenceException, OrderNotFoundException {
+        return OrderDto.from(buildOrder(dao.findByMerchantIdAndId(merchantId,orderId)));
     }
 
     //needs to be changed to return List of OrderDtos
-    public List<Order> getAllOrders() throws PersistenceException {
-        List<OrderRecord> records = dao.findAll();
+    public List<Order> getAllOrders(String merchantId) throws PersistenceException {
+        List<OrderEntity> records = dao.findByMerchantId(merchantId);
         List<Order> orders = new ArrayList<>();
-        for(OrderRecord record : records){
+        for(OrderEntity record : records){
             orders.add(buildOrder(record));
         }
         return orders;
     }
 
 
-    public Order placeOrder(List<CheckoutItemRequest> cart, PaymentMethod paymentMethod) throws PersistenceException, InsufficientStockException {
+    public Order placeOrder(String merchantId, List<CheckoutItemRequest> cart, PaymentMethod paymentMethod) throws PersistenceException, InsufficientStockException {
+        if(cart.isEmpty()) throw new IllegalArgumentException("Empty cart");
+
+
         List<LineItem> items = new ArrayList<>();
         for(CheckoutItemRequest item : cart){
-            Product p = inventoryService.getProduct(item.productId());
+            Product p = inventoryService.getProduct(merchantId,item.productId());
             items.add(new LineItem(p,item.quantity()));
         }
 
 
         // check stock
         for(LineItem item : items){
-            boolean isInStock = inventoryService.isInStock(item.getProductId(), item.getQuantity());
+            boolean isInStock = inventoryService.isInStock(merchantId, item.getProductId(), item.getQuantity());
             if(!isInStock){
                 throw new InsufficientStockException("Not enough stock for product " + item.getProductId());
             }
         }
         //create order
-        Order order = new Order(items);
+        Order order = new Order(merchantId, items);
         try {
             auditService.logAttempt(AuditType.ORDER, AuditAction.CREATE, order.getId());
 
             // create payment
             Payment payment = paymentService.createPayment(order.getTotal(), paymentMethod);
             order.setPayment(payment);
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logSuccess(AuditType.ORDER, AuditAction.CREATE, order.getId());
             return order;
         } catch (PersistenceException | InsufficientStockException e) {
             order.cancel();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logFailure(AuditType.ORDER, AuditAction.CREATE, order.getId(), e.getMessage());
 
             //we throw e here (and in other methods to do with order flow)
@@ -87,6 +93,7 @@ public class OrderService {
             throw e;
         }
     }
+
     public void authorizePayment(Order order) throws PersistenceException {
         if(order.getStatus() != OrderStatus.CREATED){
             throw new IllegalStateException("Order not in CREATED state");
@@ -98,21 +105,21 @@ public class OrderService {
 
             if(payment.getStatus() == PaymentStatus.AUTHORIZED){
                 order.markReady();
-                dao.save(toRecord(order));
+                dao.save(toEntity(order));
                 auditService.logSuccess(AuditType.ORDER, AuditAction.AUTH, order.getId());
             } else {
                 order.cancel();
-                dao.save(toRecord(order));
+                dao.save(toEntity(order));
                 auditService.logFailure(AuditType.ORDER, AuditAction.AUTH, order.getId(), "PAYMENT_DECLINED");
             }
         } catch (PersistenceException e){
             order.cancel();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logFailure(AuditType.ORDER,AuditAction.AUTH, order.getId(), e.getMessage());
             throw e;
         }
-
     }
+
     public void reserveStock(Order order) throws PersistenceException {
         if(order.getStatus() != OrderStatus.READY){
             throw new IllegalStateException("Order not in READY state");
@@ -124,15 +131,15 @@ public class OrderService {
 
             List<LineItem> items = order.getItems();
             for(LineItem item : items){
-                inventoryService.reduceStock(item.getProductId(), item.getQuantity());
+                inventoryService.reduceStock(order.getMerchantId(), item.getProductId(), item.getQuantity());
             }
             order.markReserved();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logSuccess(AuditType.ORDER, AuditAction.REDUCE_STOCK, order.getId());
 
-        } catch (PersistenceException e){
+        } catch (InsufficientStockException | PersistenceException e){
             order.cancel();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logFailure(AuditType.ORDER, AuditAction.REDUCE_STOCK, order.getId(), e.getMessage());
             throw e;
         }
@@ -150,23 +157,24 @@ public class OrderService {
             paymentService.capturePayment(payment);
             if(payment.getStatus() == PaymentStatus.CAPTURED){
                 order.markPaid();
-                dao.save(toRecord(order));
+                dao.save(toEntity(order));
                 auditService.logSuccess(AuditType.ORDER, AuditAction.CAPTURE, order.getId());
             } else {
                 order.cancel();
-                dao.save(toRecord(order));
+                dao.save(toEntity(order));
                 auditService.logFailure(AuditType.ORDER, AuditAction.CAPTURE, order.getId(), "NO_REASON_YET");
             }
 
         } catch (PersistenceException e){
             order.cancel();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logFailure(AuditType.ORDER, AuditAction.CAPTURE, order.getId(), e.getMessage());
             throw e;
         }
     }
-    public OrderDto fulfil(String orderId) throws PersistenceException, OrderNotFoundException {
-        Order order = buildOrder(dao.findById(orderId));
+
+    public OrderDto fulfil(String merchantId, String orderId) throws PersistenceException, OrderNotFoundException {
+        Order order = buildOrder(dao.findByMerchantIdAndId(merchantId, orderId));
         if(order.getStatus() != OrderStatus.PAID){
             throw new IllegalStateException("Order not in PAID state");
         }
@@ -176,18 +184,19 @@ public class OrderService {
             //any additional business logic in fulfilling order will go here
             // then we will need to split off into if success, else (like other methods)
             order.markFulfilled();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logSuccess(AuditType.ORDER, AuditAction.FULFIL, orderId);
 
         } catch (PersistenceException e){
             order.cancel();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logFailure(AuditType.ORDER, AuditAction.FULFIL, orderId, e.getMessage());
         }
         return OrderDto.from(order);
     }
-    public OrderDto complete(String orderId) throws PersistenceException, OrderNotFoundException {
-        Order order = buildOrder(dao.findById(orderId));
+
+    public OrderDto complete(String merchantId, String orderId) throws PersistenceException, OrderNotFoundException {
+        Order order = buildOrder(dao.findByMerchantIdAndId(merchantId, orderId) );
         if(order.getStatus() != OrderStatus.FULFILLED){
             throw new IllegalStateException("Order not in FULFILLED state");
         }
@@ -198,41 +207,45 @@ public class OrderService {
             // cant complete order
 
             order.markCompleted();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logSuccess(AuditType.ORDER, AuditAction.COMPLETE, orderId);
 
         } catch (PersistenceException e){
             order.cancel();
-            dao.save(toRecord(order));
+            dao.save(toEntity(order));
             auditService.logFailure(AuditType.ORDER, AuditAction.COMPLETE, orderId, e.getMessage());
         }
         return OrderDto.from(order);
     }
 
-    private Order buildOrder(OrderRecord record) throws PersistenceException {
-        Payment payment = paymentService.getPayment(record.paymentId());
+    private Order buildOrder(OrderEntity entity) throws PersistenceException {
+        Payment payment = paymentService.getPayment(entity.getPaymentId());
         List<LineItem> items = new ArrayList<>();
-        for(LineItemRecord itemRecord : record.items()){
-            Product product = inventoryService.getProduct(itemRecord.productId());
-            LineItem item = new LineItem(product,itemRecord.quantity(),itemRecord.price());
+        for(LineItemEntity itemEntity : entity.getItems()){
+            Product product = inventoryService.getProduct(entity.getMerchantId(), itemEntity.getProductId());
+            LineItem item = new LineItem(product,itemEntity.getQuantity(), itemEntity.getPrice());
             items.add(item);
         }
-        OrderStatus status = OrderStatus.values()[record.statusOrdinal()];
+        OrderStatus status = entity.getStatus();
 
-        return Order.fromPersistence(record.id(), items, payment, status);
+        return new Order(entity.getId(), entity.getMerchantId(), payment, items, status);
     }
-    private OrderRecord toRecord(Order order){
-        List<LineItemRecord> items = new ArrayList<>();
+    private OrderEntity toEntity(Order order){
 
+        OrderEntity entity = new OrderEntity(order.getId(), order.getMerchantId(), order.getStatus(), order.getPayment().getId());
+
+        List<LineItemEntity> items = new ArrayList<>();
         for (LineItem item : order.getItems()) {
-            items.add(new LineItemRecord(
+            items.add(new LineItemEntity(
                     item.getProductId(),
                     item.getQuantity(),
                     item.getPriceAtPurchase()
             ));
         }
 
-        return new OrderRecord(order.getId(), order.getStatus().ordinal(), order.getPayment().getId(), items);
+        entity.setItems(items);
+
+        return entity;
     }
 
 }
